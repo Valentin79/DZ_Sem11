5d10dc617e1c971740f028add7ae2e93498ca559
Valentin <zvezdochet.79@mail.ru>
2022-04-18 15:10:33 +0300

    задачи 1,2,2.1

---
+++ Задача1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+bool DegreNumber(double N)
+{
+    if(N%2 != 0) return false;
+    if(N == 2) return true;
+    return DegreNumber(N/2);
+    
+}
+
+int N = Prompt("Введите число: ");
+if(DegreNumber(N) == true) Console.WriteLine($"{N} является степенью двойки");
+if(DegreNumber(N) == false) Console.WriteLine($"{N} не является степенью двойки");
---
+++ Задача2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Проверка на простое число:
+// N = 13 -> "Это простое число"
+// N = 12 -> “Это не простое число”
+
+// 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+bool SimpleNumber(int N)
+{
+    if(N <= -2) N = N*-1;
+    if(N == 2) return true;
+    for(int i = 2; i < N; i++)
+    {
+        if(N % i == 0) return false;
+    }
+    return true;
+}
+
+int N = Prompt("Введите число: ");
+while(N < 2 && N > -2)
+{
+    N = Prompt("Введите число больше 1: ");
+}
+
+if(SimpleNumber(N) == true) Console.WriteLine($"{N} является простым числом");
+if(SimpleNumber(N) == false) Console.WriteLine($"{N} не является простым числом");
